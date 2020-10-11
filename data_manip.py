df45f82c8e65a5803e849351b28bf438f440cd89
Max <mpage-sowik@outlook.com>
2020-10-11 16:52:37 -0400

    completed the performance added metrics

--- data_manip.py
+++ data_manip.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 def read_indexed(filename):
@@ -15,4 +16,18 @@ def read_unindexed(filename):
 
 def write_indexed(filename, df):
     df.to_csv(filename, header=None)
-        
+
+def add_performance(filename, data, perf_type=""):
+    with open(filename,mode='a') as f:
+        if isinstance(data,float):
+            f.write(perf_type + ',' + str(data) + '\n')
+        else:
+            for index in range(0,len(data)):
+                f.write(str(index) + ',' + str(data[index]) + '\n')
+
+def add_class_measures(filename,data):
+    with open(filename,mode='a') as f:
+        f.write('class,precision,recall,fscore,total\n')
+        for index in range(0,len(data[0])):
+            f.write(str(index) + ',' + str(data[0][index])+ ',' + str(data[1][index])+ ',' + str(data[2][index])+ ',' + str(data[3][index])+'\n')
+   
--- m_learner.py
+++ m_learner.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import zeros_like
 from numpy.lib.function_base import average
 import data_manip
 import numpy as np
@@ -12,23 +13,23 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 
 def gnb_predictor(ver):
     X,Y = data_manip.read_indexed("./data/train_"+str(ver)+".csv")
-    X_test = data_manip.read_unindexed("./data/test_no_label_"+str(ver)+".csv")
+    X_test, Y_test = data_manip.read_indexed("./data/test_with_label_"+str(ver)+".csv")
     gnb = GaussianNB()
     gnb.fit(X,Y)
-    output_arr = []
-    for i in range(0,len(X_test)) :
-        output_arr.append(gnb.predict([X_test[i]])[0])
+    output_arr = [gnb.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/GNB-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
+    calculate_metrics(Y_test,output_arr,ver,"./output/GNB-DS"+str(ver)+".csv")
+
 
 def base_dt(ver):
     X,Y = data_manip.read_indexed("./data/train_"+str(ver)+".csv")
-    X_test = data_manip.read_unindexed("./data/test_no_label_"+str(ver)+".csv")
+    X_test, Y_test = data_manip.read_indexed("./data/test_with_label_"+str(ver)+".csv")
     dtc = tree.DecisionTreeClassifier()
     dtc.fit(X,Y)
-    output_arr = []
-    for i in range(0,len(X_test)) :
-        output_arr.append(dtc.predict([X_test[i]])[0])
+    output_arr = [dtc.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/Base-DT-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
+    calculate_metrics(Y_test,output_arr,ver,"./output/Base-DT-DS"+str(ver)+".csv")
+
 
 def best_dt(ver):
     """
@@ -47,22 +48,29 @@ def best_dt(ver):
     dtc.min_impurity_decrease=0.00025
     dtc.class_weight=None
     dtc.fit(X,Y)
-    output_arr = []
-    for i in range(0,len(X_test)) :
-        output_arr.append(dtc.predict([X_test[i]])[0])
-    calculate_metrics(Y_test,output_arr)
+    output_arr = [dtc.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/Best-DT-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
+    calculate_metrics(Y_test,output_arr,ver,"./output/Best-DT-DS"+str(ver)+".csv")
 
-def calculate_metrics(actual,expected):
-    print(confusion_matrix(actual,expected))
+
+def calculate_metrics(expected,actual,ver,fname):
+    if ver == 1:
+        data_manip.add_performance(fname,confusion_matrix(expected,actual,labels=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]))
+    else:
+        data_manip.add_performance(fname,confusion_matrix(expected,actual,labels=[0,1,2,3,4,5,6,7,8,9]))
+
 
     # print(recall_score(actual,expected,average='weighted'))
     # print(precision_score(actual,expected,average='weighted'))
-    print(precision_recall_fscore_support(actual,expected,average=None))
+    if ver == 1:
+        data_manip.add_class_measures(fname,precision_recall_fscore_support(expected,actual,average=None,zero_division=0,labels=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]))
+    else:
+        data_manip.add_class_measures(fname,precision_recall_fscore_support(expected,actual,average=None,zero_division=0,labels=[0,1,2,3,4,5,6,7,8,9]))
 
-    print(accuracy_score(actual,expected))
-    print(f1_score(actual,expected,average='macro'))
-    print(f1_score(actual,expected,average='weighted'))
+
+    data_manip.add_performance(fname,accuracy_score(expected,actual),'accuracy')
+    data_manip.add_performance(fname,f1_score(expected,actual,average='macro'),'f1_macro')
+    data_manip.add_performance(fname,f1_score(expected,actual,average='weighted'),'f1_weighted')
 
 
 def calculate_distribution(ver):
@@ -72,30 +80,34 @@ def calculate_distribution(ver):
 
 def default_perceptron(ver):
     X,Y = data_manip.read_indexed("./data/train_"+str(ver)+".csv")
-    X_test = data_manip.read_unindexed("./data/test_no_label_"+str(ver)+".csv")
+    X_test, Y_test = data_manip.read_indexed("./data/test_with_label_"+str(ver)+".csv")
     per = Perceptron() # Default params
     per.fit(X, Y)
     output_arr = [per.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/PER-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
+    calculate_metrics(Y_test,output_arr,ver,"./output/PER-DS"+str(ver)+".csv")
 
 def base_multi_layered_perceptron(ver):
     X,Y = data_manip.read_indexed("./data/train_"+str(ver)+".csv")
-    X_test = data_manip.read_unindexed("./data/test_no_label_"+str(ver)+".csv")
-    mlp = MLPClassifier(hidden_layer_sizes=(100,), activation="logistic", solver='sgd')
+    X_test, Y_test = data_manip.read_indexed("./data/test_with_label_"+str(ver)+".csv")
+    mlp = MLPClassifier(hidden_layer_sizes=(100,), activation="logistic", solver='sgd',max_iter=400)
     mlp.fit(X, Y)
     output_arr = [mlp.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/Base-MLP-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
+    calculate_metrics(Y_test,output_arr,ver,"./output/Base-MLP-DS"+str(ver)+".csv")
 
 def best_multi_layered_perceptron(ver):
     X,Y = data_manip.read_indexed("./data/train_"+str(ver)+".csv")
-    X_test = data_manip.read_unindexed("./data/test_no_label_"+str(ver)+".csv")
-    mlp = MLPClassifier()
+    X_test, Y_test = data_manip.read_indexed("./data/test_with_label_"+str(ver)+".csv")
+    mlp = MLPClassifier(max_iter=400)
     parameters = {
-        'hidden_layer_sizes': [(30, 50), (10, 10 ,10)],
-        'activation': ['identity', 'logistic', 'tanh', 'relu'],
-        'solver': ['sgd', 'adam']
+        'hidden_layer_sizes': [(30,30,30) ,(100,)],
+        'activation': ['relu'],
+        'solver': ['sgd']
     }   
     gridMLP = GridSearchCV(mlp, parameters)
     gridMLP.fit(X, Y)
     output_arr = [gridMLP.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/Best-MLP-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
+    calculate_metrics(Y_test,output_arr,ver,"./output/Best-MLP-DS"+str(ver)+".csv")
+
