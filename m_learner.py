fd03f59dadabf19fd2f4584c052d7329675e6a51
Max <mpage-sowik@outlook.com>
2020-10-13 21:56:08 -0400

    added plot images

--- m_learner.py
+++ m_learner.py
@@ -1,8 +1,12 @@
+from matplotlib.pyplot import yticks
 from numpy.core.numeric import zeros_like
 from numpy.lib.function_base import average
 import data_manip
+import os
 import numpy as np
 import pandas as pd
+from matplotlib.ticker import MultipleLocator
+import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
 from sklearn.linear_model import Perceptron
@@ -110,4 +114,53 @@ def best_multi_layered_perceptron(ver):
     output_arr = [gridMLP.predict([X])[0] for X in X_test]
     data_manip.write_indexed("./output/Best-MLP-DS"+str(ver)+".csv",pd.DataFrame({'index':output_arr}))
     calculate_metrics(Y_test,output_arr,ver,"./output/Best-MLP-DS"+str(ver)+".csv")
+
+def plot_lib():
+    folder = "./plot_data"
+    # ax = fig.add_subplot(1,1,1)
+    bar_width=0.15
+    pos2 = 0.0
+    pos = 0.0
+
+
+    for file in os.listdir(folder):
+        df = pd.read_csv(os.path.join(folder,file))
+
+        if '1' in file:
+            plot_bars(1,df['fscore'],pos,bar_width,df['class'],file.split('.')[0])
+            pos+=(bar_width)
+
+            # plt.figure(1, (10,100))
+            # df['precision'] = pd.Series([(val*100) for val in df['precision']])
+            # plt.xticks(np.arange(0,26,1))
+            # plt.yticks(np.arange(0,110,10))
+            # plt.plot(df['precision'])
+        else:
+            plot_bars(2,df['fscore'],pos2,bar_width,df['class'],file.split('.')[0])
+            pos2+=(bar_width)
+
+            # plt.figure(2, (10,100))
+            # plt.xticks(np.arange(0,10,1))
+            # plt.yticks(np.arange(0,110,10))
+            # plt.bar(df['class'],df['precision'],label=file)#.plot(df['precision'])
+
+    plt.show()
+
+def plot_bars(ver,data,pos,bar_width,labels,algo_name):
+        initial = np.arange(len(labels))
+        fig = plt.figure(ver,(20,100))
+        ax = plt.subplot(111)
+        title = data.name
+        data= pd.Series([(val*100) for val in data])
+        ax.bar(initial+pos,data,width=bar_width,label=algo_name)
+        ax.set_xticks(initial+(3*bar_width))
+        ax.set_xticklabels(labels)
+        ax.set_title(title)
+        plt.yticks(np.arange(0,105,5))
+        ml = MultipleLocator(5)
+        ax.yaxis.set_minor_locator(ml)
+        # plt.minorticks_on()
+        ax.tick_params(axis='x', which='minor', bottom=False)
+        ax.legend()
+
 
